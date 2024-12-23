2e9cb892936e39ef7597e3a88188772630e6723f
Rakshith Raj Satrasala <rakshithrajr@gmail.com>
2024-12-23 14:28:43 +0530

    fix: import React explicitly in menuItems component

--- client/src/components/menu/menuItems.jsx
+++ client/src/components/menu/menuItems.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import rupee from "../../utils/currencyFormatter.jsx";
 import { capitalize } from "../../utils/capitalize.jsx";
